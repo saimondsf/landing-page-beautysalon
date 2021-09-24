67594cda50b421c323132a7b14b206442325ac0a
Saimon Davi <saimon.dsfg@gmail.com>
2021-09-24 15:00:03 -0300

    Trocando o Swiper pelo Owl

--- main.js
+++ main.js
@@ -32,20 +32,20 @@ function changeHeaderWhenScroll() {
 }
 
 /* Testimonials carousel slider swiper */
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      setWrapperSize: true
-    }
-  }
-})
+// const swiper = new Swiper('.swiper-container', {
+//   slidesPerView: 1,
+//   pagination: {
+//     el: '.swiper-pagination'
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+//   breakpoints: {
+//     767: {
+//       slidesPerView: 2,
+//       setWrapperSize: true
+//     }
+//   }
+// })
 
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
